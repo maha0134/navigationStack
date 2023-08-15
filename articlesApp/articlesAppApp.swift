30b6104ef2397dc4bd3a4f4a2ef30a82a92c27f9
Akshay Mahajan <maha0134@algonquinlive.com>
2023-08-15 12:16:38 +0530

    Initial Commit

---
+++ articlesApp/articlesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  articlesAppApp.swift
+//  articlesApp
+//
+//  Created by AKSHAY MAHAJAN on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct articlesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
